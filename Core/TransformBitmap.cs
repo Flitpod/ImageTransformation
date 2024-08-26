fdcb81c8330fe89d628b472f65d179c6c6078a3a
Toth, David4 (ext) (ADV D EU HU OPS 3 TDP 3) <david4.toth@evosoft.com>
2024-08-26 08:22:16 +0200

    Reorganize names and projects in solution

--- Core/TransformBitmap.cs
+++ Core/TransformBitmap.cs
@@ -116,6 +116,115 @@ namespace Core
             // get the inverse of the transformation
             Matrix invTransformation = transformation.GetInverse();
 
+            // if interpolation type is Floating_FromSource
+            if(interpolationType == InterpolationTypes.Floating_FromSource)
+            {
+                // create bitmap from source with 1 pixel padding
+                Bitmap sourcePadded = new Bitmap(source.Width + 2, source.Height + 2);
+
+                // calculation of the transformation
+                {
+                    // get data for transformation
+                    int heightR = result.Height;
+                    int widthR = result.Width;
+                    int heightS = sourcePadded.Height;
+                    int widthS = sourcePadded.Width;
+                    BitmapData bitmapDataSrcPadded = sourcePadded.LockBits(new Rectangle(0, 0, widthS, heightS), ImageLockMode.ReadWrite, PixelFormat.Format32bppRgb);
+                    BitmapData bitmapDataDest = result.LockBits(new Rectangle(0, 0, widthR, heightR), ImageLockMode.ReadWrite, PixelFormat.Format32bppRgb);
+                    int strideS = bitmapDataSrcPadded.Stride;
+                    int strideR = bitmapDataDest.Stride;
+                    byte* ptrSrc0 = (byte*)bitmapDataSrcPadded.Scan0;
+                    byte* ptrDest0 = (byte*)bitmapDataDest.Scan0;
+                    int pixelFormat = 4;
+
+                    // copy the source image content into the padded image
+                    {
+                        BitmapData bitmapDataSrc = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppRgb);
+                        byte* ptrSrcOrig0 = (byte*)bitmapDataSrc.Scan0;
+                        byte* ptrDestPadded0 = (byte*)(bitmapDataSrcPadded.Scan0 + strideS + pixelFormat);
+                        Parallel.For(0, heightR, (row) =>
+                        {
+                            for (int col = 0; col < widthR; col++)
+                            {
+                                byte* ptrSrc = (byte*)(ptrSrcOrig0 + row * strideR + col * pixelFormat);
+                                byte* ptrDest = (byte*)(ptrDestPadded0 + row * strideS + col * pixelFormat);
+                                ptrDest[0] = ptrSrc[0];
+                                ptrDest[1] = ptrSrc[1];
+                                ptrDest[2] = ptrSrc[2];
+                            }
+                        });
+                        source.UnlockBits(bitmapDataSrc);
+                    }
+
+                    Parallel.For(0, heightR, (row) =>
+                    //for(int row = 0; row < heightR; row++) 
+                    {
+                        for (int col = 0; col < widthR; col++)
+                        {
+                            // pointer to the actual pixel on the destination bitmap
+                            byte* ptrDest = (byte*)(ptrDest0 + row * strideR + col * pixelFormat);
+
+                            Matrix coordsDest = new Matrix()
+                            {
+                                Values = new double[3, 1]
+                                {
+                                { row }, // row
+                                { col }, // col
+                                { 1 }  // homogenious
+                                }
+                            };
+
+                            // calcuate the destination cordinates (pixels)
+                            Matrix coordsSrc = invTransformation * coordsDest;
+
+                            // Calculate the coordinates of the source pixels
+                            int rowSrc = (int)(coordsSrc[0, 0] + 1);
+                            int colSrc = (int)(coordsSrc[1, 0] + 1);
+                            double rowSrcF = coordsSrc[0, 0] + 1;
+                            double colSrcF = coordsSrc[1, 0] + 1;
+
+                            // inside the image
+                            if (rowSrcF >= 0 && rowSrcF < (heightS - 1) && colSrcF >= 0 && colSrcF < (widthS - 1))
+                            {
+                                // pointer to the source pixel and for the surronding pixels
+                                byte* ptrSrc = (byte*)(ptrSrc0 + rowSrc * strideS + colSrc * pixelFormat);
+
+                                // weights for neighbouring pixels
+                                double rowSrcF_Down = rowSrcF - rowSrc;
+                                double rowSrcF_Up = 1 - rowSrcF_Down;
+                                double colSrcF_Right = colSrcF - colSrc;
+                                double colSrcF_Left = 1 - colSrcF_Right;
+                                double[] weights = new double[4]
+                                {
+                                rowSrcF_Up * colSrcF_Left,    // (0 - [up,left]);
+                                rowSrcF_Up * colSrcF_Right,   // (1 - [up,right]);
+                                rowSrcF_Down * colSrcF_Left,  // (2 - [down,left]);
+                                rowSrcF_Down * colSrcF_Right  // (3 - [down,right]);
+                                };
+
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    ptrDest[i] = (byte)(weights[0] * ptrSrc[i]
+                                                      + weights[1] * ptrSrc[pixelFormat + i]
+                                                      + weights[2] * ptrSrc[strideS + i]
+                                                      + weights[3] * ptrSrc[strideS + pixelFormat + i]);
+                                }
+                            }
+                            // outside of 1 pixel extra margin around the image
+                            else
+                            {
+                                for (int i = 0; i < 3; i++)
+                                    ptrDest[i] = 0;
+                            }
+                        }
+                    });
+
+                    sourcePadded.UnlockBits(bitmapDataSrcPadded);
+                    result.UnlockBits(bitmapDataDest);
+                }
+                return;
+            }
+
             // calculation of the transformation
             {
                 // get data for transformation
@@ -140,9 +249,9 @@ namespace Core
                         {
                             Values = new double[3, 1]
                             {
-                            { row }, // row
-                            { col }, // col
-                            { 1 }  // homogenious
+                                { row }, // row
+                                { col }, // col
+                                { 1 }  // homogenious
                             }
                         };
 
@@ -167,98 +276,98 @@ namespace Core
                                 for (int i = 0; (i < 3); i++)
                                     ptrDest[i] = 0;
                         }
-                        else if (interpolationType == InterpolationTypes.Floating_FromSource)
-                        {
-                            // pointer to the source pixel and for the surronding pixels
-                            ptrSrc = (byte*)(ptrSrc0 + rowSrc * stride + colSrc * pixelFormat);
+                        //else if (interpolationType == InterpolationTypes.Floating_FromSource)
+                        //{
+                        //    // pointer to the source pixel and for the surronding pixels
+                        //    ptrSrc = (byte*)(ptrSrc0 + rowSrc * stride + colSrc * pixelFormat);
 
-                            // floating point coordinates
-                            double rowSrcF = coordsSrc[0, 0];
-                            double colSrcF = coordsSrc[1, 0];
+                        //    // floating point coordinates
+                        //    double rowSrcF = coordsSrc[0, 0];
+                        //    double colSrcF = coordsSrc[1, 0];
 
-                            // casted to floor integer coordinates
-                            rowSrc = (int)rowSrcF;
-                            colSrc = (int)colSrcF;
+                        //    // casted to floor integer coordinates
+                        //    rowSrc = (int)rowSrcF;
+                        //    colSrc = (int)colSrcF;
 
-                            // set pointer to source pixel
-                            ptrSrc = (byte*)(ptrSrc0 + rowSrc * stride + colSrc * pixelFormat);
+                        //    // set pointer to source pixel
+                        //    ptrSrc = (byte*)(ptrSrc0 + rowSrc * stride + colSrc * pixelFormat);
 
-                            // outside of 1 pixel extra margin around the image
-                            if (rowSrcF <= -1 || rowSrcF >= height || colSrcF <= -1 || colSrcF >= width)
-                            {
-                                for (int i = 0; i < 3; i++)
-                                    ptrDest[i] = 0;
-                            }
-                            // inside the image
-                            else if (rowSrcF >= 0 && rowSrcF < (height - 1) && colSrcF >= 0 && colSrcF < (width - 1))
-                            {
-                                // weights for neighbouring pixels
-                                double rowSrcF_Down = rowSrcF - rowSrc;
-                                double rowSrcF_Up = 1 - rowSrcF_Down;
-                                double colSrcF_Right = colSrcF - colSrc;
-                                double colSrcF_Left = 1 - colSrcF_Right;
-                                double[] weights = new double[4]
-                                {
-                                rowSrcF_Up * colSrcF_Left,    // (0 - [up,left]);
-                                rowSrcF_Up * colSrcF_Right,   // (1 - [up,right]);
-                                rowSrcF_Down * colSrcF_Left,  // (2 - [down,left]);
-                                rowSrcF_Down * colSrcF_Right  // (3 - [down,right]);
-                                };
+                        //    // outside of 1 pixel extra margin around the image
+                        //    if (rowSrcF <= -1 || rowSrcF >= height || colSrcF <= -1 || colSrcF >= width)
+                        //    {
+                        //        for (int i = 0; i < 3; i++)
+                        //            ptrDest[i] = 0;
+                        //    }
+                        //    // inside the image
+                        //    else if (rowSrcF >= 0 && rowSrcF < (height - 1) && colSrcF >= 0 && colSrcF < (width - 1))
+                        //    {
+                        //        // weights for neighbouring pixels
+                        //        double rowSrcF_Down = rowSrcF - rowSrc;
+                        //        double rowSrcF_Up = 1 - rowSrcF_Down;
+                        //        double colSrcF_Right = colSrcF - colSrc;
+                        //        double colSrcF_Left = 1 - colSrcF_Right;
+                        //        double[] weights = new double[4]
+                        //        {
+                        //        rowSrcF_Up * colSrcF_Left,    // (0 - [up,left]);
+                        //        rowSrcF_Up * colSrcF_Right,   // (1 - [up,right]);
+                        //        rowSrcF_Down * colSrcF_Left,  // (2 - [down,left]);
+                        //        rowSrcF_Down * colSrcF_Right  // (3 - [down,right]);
+                        //        };
 
-                                for (int i = 0; i < 3; i++)
-                                {
-                                    ptrDest[i] = (byte)(weights[0] * ptrSrc[i]
-                                                      + weights[1] * ptrSrc[pixelFormat + i]
-                                                      + weights[2] * ptrSrc[stride + i]
-                                                      + weights[3] * ptrSrc[stride + pixelFormat + i]);
-                                }
-                            }
-                            // inside the 1 pixel width margin 
-                            else
-                            {
-                                // corners
-                                if ((rowSrcF < 0 && colSrcF < 0) || (rowSrcF < 0 && colSrcF > (width - 1)) || (rowSrcF > (height - 1) && colSrcF < 0) || (rowSrcF > (height - 1) && colSrcF > (width - 1)))
-                                {
-                                    for (int i = 0; i < 3; i++)
-                                        ptrDest[i] = (byte)(ptrSrc[i] * 0.25);
-                                }
-                                // above first row or below last row
-                                else if (rowSrcF < 0 || rowSrcF > (height - 1))
-                                {
-                                    double weightRight = colSrcF - colSrc;
-                                    double weightLeft = 1 - weightRight;
-                                    for (int i = 0; i < 3; i++)
-                                        ptrDest[i] = (byte)(weightLeft * ptrSrc[i] + weightRight * ptrSrc[pixelFormat + i]);
-                                }
-                                // before first col or after last col
-                                else if (colSrcF < 0 || colSrcF > (width - 1))
-                                {
-                                    // weights for neighbouring pixels
-                                    double rowSrcF_Down = rowSrcF - rowSrc;
-                                    double rowSrcF_Up = 1 - rowSrcF_Down;
-                                    double colSrcF_Right = colSrcF - colSrc;
-                                    double colSrcF_Left = 1 - colSrcF_Right;
-                                    double[] weights = new double[4]
-                                    {
-                                        rowSrcF_Up * colSrcF_Left,    // (0 - [up,left]);
-                                        rowSrcF_Up * colSrcF_Right,   // (1 - [up,right]);
-                                        rowSrcF_Down * colSrcF_Left,  // (2 - [down,left]);
-                                        rowSrcF_Down * colSrcF_Right  // (3 - [down,right]);
-                                    };
+                        //        for (int i = 0; i < 3; i++)
+                        //        {
+                        //            ptrDest[i] = (byte)(weights[0] * ptrSrc[i]
+                        //                              + weights[1] * ptrSrc[pixelFormat + i]
+                        //                              + weights[2] * ptrSrc[stride + i]
+                        //                              + weights[3] * ptrSrc[stride + pixelFormat + i]);
+                        //        }
+                        //    }
+                        //    // inside the 1 pixel width margin 
+                        //    else
+                        //    {
+                        //        // corners
+                        //        if ((rowSrcF < 0 && colSrcF < 0) || (rowSrcF < 0 && colSrcF > (width - 1)) || (rowSrcF > (height - 1) && colSrcF < 0) || (rowSrcF > (height - 1) && colSrcF > (width - 1)))
+                        //        {
+                        //            for (int i = 0; i < 3; i++)
+                        //                ptrDest[i] = (byte)(ptrSrc[i] * 0.25);
+                        //        }
+                        //        // above first row or below last row
+                        //        else if (rowSrcF < 0 || rowSrcF > (height - 1))
+                        //        {
+                        //            double weightRight = colSrcF - colSrc;
+                        //            double weightLeft = 1 - weightRight;
+                        //            for (int i = 0; i < 3; i++)
+                        //                ptrDest[i] = (byte)(weightLeft * ptrSrc[i] + weightRight * ptrSrc[pixelFormat + i]);
+                        //        }
+                        //        // before first col or after last col
+                        //        else if (colSrcF < 0 || colSrcF > (width - 1))
+                        //        {
+                        //            // weights for neighbouring pixels
+                        //            double rowSrcF_Down = rowSrcF - rowSrc;
+                        //            double rowSrcF_Up = 1 - rowSrcF_Down;
+                        //            double colSrcF_Right = colSrcF - colSrc;
+                        //            double colSrcF_Left = 1 - colSrcF_Right;
+                        //            double[] weights = new double[4]
+                        //            {
+                        //                rowSrcF_Up * colSrcF_Left,    // (0 - [up,left]);
+                        //                rowSrcF_Up * colSrcF_Right,   // (1 - [up,right]);
+                        //                rowSrcF_Down * colSrcF_Left,  // (2 - [down,left]);
+                        //                rowSrcF_Down * colSrcF_Right  // (3 - [down,right]);
+                        //            };
 
-                                    double weightUp = 0;
-                                    double weightDown = 0;
+                        //            double weightUp = 0;
+                        //            double weightDown = 0;
 
-                                    for (int i = 0; i < 3; i++)
-                                    {
-                                        ptrDest[i] = (byte)(weights[0] * ptrSrc[i]
-                                                          + weights[1] * ptrSrc[pixelFormat + i]
-                                                          + weights[2] * ptrSrc[stride + i]
-                                                          + weights[3] * ptrSrc[stride + pixelFormat + i]);
-                                    }
-                                }
-                            }
-                        }
+                        //            for (int i = 0; i < 3; i++)
+                        //            {
+                        //                ptrDest[i] = (byte)(weights[0] * ptrSrc[i]
+                        //                                  + weights[1] * ptrSrc[pixelFormat + i]
+                        //                                  + weights[2] * ptrSrc[stride + i]
+                        //                                  + weights[3] * ptrSrc[stride + pixelFormat + i]);
+                        //            }
+                        //        }
+                        //    }
+                        //}
                         else if (interpolationType == InterpolationTypes.Dir4_FromSource)
                         {
                             int[] pixelValues = new int[3];
