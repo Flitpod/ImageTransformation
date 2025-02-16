61c8ef234013da45c8a04d3d1312a22eb23a01e7
David Toth <flitpod@outlook.com>
2025-02-16 22:13:00 +0200

    Core - Transformation related project tree refactor

---
+++ Core/Helpers/DoubleExtender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class DoubleExtender
+    {
+        public static double ToRadian(this double degree)
+        {
+            return Math.PI * (degree / 180);
+        }
+    }
+}
--- Core/LinearTransformations/Transformations.cs
+++ Core/LinearTransformations/Transformations.cs
@@ -8,13 +8,6 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    public static class DoubleExtender
-    {
-        public static double ToRadian(this double degree)
-        {
-            return Math.PI * (degree / 180);
-        }
-    }
     public static class Transformations
     {
         public static Matrix Rotation(double degree, Dimension dimension)
