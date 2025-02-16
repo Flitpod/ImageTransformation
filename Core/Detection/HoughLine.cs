e9880f074af999cc8198bcc05a9621273a36727b
David Toth <flitpod@outlook.com>
2025-02-16 22:14:09 +0200

    Core - HoughLine - Steps description added

---
+++ Core/Detection/HoughLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Detection
+{
+    public class HoughLine
+    {
+        // STEPS
+        // 1. Apply Canny on RGB image
+        // 2. Create Accumulator array and fill with votes based on Canny edge detected image
+        // 3. Filter out the first nth highest vote and return them as IEnumerable<LinePolar> descending by votes
+
+    }
+}
