a18cd38ba5a76f6fce0e0c151088d4b694c25122
David Toth <flitpod@outlook.com>
2025-10-13 22:11:21 +0300

    NativeCore - Added methods to Matrix source file with TODO - implement as placeholders to match with the header file.

--- NativeCore/Matrix.cpp
+++ NativeCore/Matrix.cpp
@@ -21,7 +21,8 @@ namespace NativeCore {
 
 	// ctors
 	Matrix::Matrix()
-		: m_Rows(0), m_Cols(0) {}
+		: m_Rows(0), m_Cols(0) {
+	}
 	Matrix::Matrix(int rows, int cols)
 		: m_Rows(rows), m_Cols(cols)
 	{
@@ -90,5 +91,37 @@ namespace NativeCore {
 		}
 
 		return result;
+	}
+
+	// --- TODO - Add implementation ---
+	Matrix Matrix::GetAdjointMatrix() const {
+		// TODO: implementation - private
+	}
+
+	Matrix Matrix::GetSubMatrixWithoutRowAndCol(int row, int col) const {
+		// TODO: implementation - private
+	}
+
+	Matrix Matrix::GetInverse() const {
+		// TODO: implementation - public
+	}
+
+	double Matrix::GetDeterminant() const {
+		// TODO: implementation - public
+	}
+
+	// TODO: Add unit tests
+	Matrix Matrix::GetTranspose() const {
+		Matrix result(m_Cols, m_Rows);
+		for (int row = 0; row < m_Rows; row++) {
+			for (int col = 0; col < m_Cols; col++) {
+				result(col, row) = (*this)(row, col);
+			}
+		}
+		return result;
+	}
+
+	Matrix Matrix::GetReducedEchelonForm(int& singForDeterminant) const {
+		// TODO: implementation - public
 	}
 }
